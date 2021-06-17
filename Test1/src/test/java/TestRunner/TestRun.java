6b1e9d4e7a7e3d9a7efde48487d5fb86bf336aa5
sushils <sushils@SNL011025058.infics.com>
2021-06-17 15:11:29 +0530

    add new class

--- Test1/src/test/java/TestRunner/TestRun.java
+++ Test1/src/test/java/TestRunner/TestRun.java
@@ -4,12 +4,13 @@ import org.junit.runner.RunWith;
 
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
+//import cucumber.api.en.junit.Cucumber;
 
 
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-				features= {"C:\\Users\\sushils\\eclipse-workspace\\Test1\\features\\Login.feature"},
+				features= {"C:\\Users\\sushils\\git\\SeleniumJavaFreamwork1\\Test1\\features\\Login.feature"},
 				glue= {"StepDefination"},
 				dryRun=true,
 				monochrome=true,
